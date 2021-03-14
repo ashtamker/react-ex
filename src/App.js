964084e10486360b0dc5417d3b7b24b929ea73d9
Or Ashtamker <ashi@MacBook-Pro.local>
2021-03-14 16:08:47 +0200

    add 4-1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Basic from './component/2.2/Basic';
 import Box1 from './component/3.1/Box1';
+import Buttons from './component/4-1/Buttons';
 
 function App() {
   return (
-   <Box1 />
+   <Buttons />
   );
 }
 
---
+++ src/component/4-1/Buttons.js
@@ -0,0 +1,12 @@
+const Buttons = () => {
+   return (
+       <div>
+           <button style={{fontWeight:'bold'}}>Important</button>
+           <button>Not Important</button>
+       </div>
+   )
+
+}
+
+
+export default Buttons;
