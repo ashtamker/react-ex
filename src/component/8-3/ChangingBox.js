22f5de78942523fa3c85a3a7f7b1831523e87366
Or Ashtamker <ashi@MacBook-Pro.local>
2021-03-16 20:09:09 +0200

    more option for 8-3

--- src/component/8-3/ChangingBox.js
+++ src/component/8-3/ChangingBox.js
@@ -35,3 +35,39 @@ import BoxBody from './BoxBody';
 }
 
 export default ChangingBox;
+
+
+
+
+// import React from 'react';
+
+// const colors = ['#535c68', '#95afc0', '#c7ecee', '#dff9fb', '#7ed6df']
+
+// class Change extends React.Component {
+//   state = { color: 0, shape: '' };
+
+//   componentDidMount() {
+//     setInterval(() => {
+//       console.log(this.state.color);
+//       if(this.state.color>3){
+//         this.setState({
+//           color: (this.state.color +1)%5, borderRadius: this.state.shape = '50%',
+//         })
+//       }else{
+//       this.setState({
+//         color: (this.state.color + 1),
+//       })}
+//     }, 500);
+//   }
+//   render() {
+//     return (
+//       <div style={{
+//         backgroundColor: colors[this.state.color],
+//         borderRadius: this.state.shape,
+//         width: '100px',
+//         height: '100px'
+//       }}></div>
+//     )
+//   };
+// };
+// export default Change
